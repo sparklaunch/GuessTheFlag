8c53f2830e45acd48f920284bdcf51b0025d5040
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-05-31 18:53:05 +0900

    Done

--- GuessTheFlag/Controllers/ViewController.swift
+++ GuessTheFlag/Controllers/ViewController.swift
@@ -18,10 +18,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.askQuestion()
     }
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        self.countries.shuffle()
         self.correctAnswer = Int.random(in: 0...2)
         self.title = self.countries[self.correctAnswer].uppercased()
-        self.countries.shuffle()
         self.button1.setImage(UIImage(named: self.countries[0]), for: .normal)
         self.button2.setImage(UIImage(named: self.countries[1]), for: .normal)
         self.button3.setImage(UIImage(named: self.countries[2]), for: .normal)
@@ -31,6 +31,20 @@ class ViewController: UIViewController {
         self.button1.layer.borderColor = UIColor.lightGray.cgColor
         self.button2.layer.borderColor = UIColor.lightGray.cgColor
         self.button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == self.correctAnswer {
+            title = "Correct"
+            self.score += 1
+        }
+        else {
+            title = "Wrong"
+            self.score -= 1
+        }
+        let alert: UIAlertController = UIAlertController(title: title, message: "Your current score is \(self.score).", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: self.askQuestion))
+        self.present(alert, animated: true)
     }
 }
 
